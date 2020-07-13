ef77abfaa0f65fab82f7d9f8a23db646497fe865
Ender Temel <ender@medusamedya.com>
2020-07-13 21:29:02 +0300

    Clicky game difficult selection, best score eklendi.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private float spawnRate = 1.0f;
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI bestScore;
+    public TextMeshProUGUI gameOverText;
+    public Button RestartButton;
+    private int score;
+    private int highScore;
+    public bool isGameActive;
+    public GameObject TitleScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+         highScore = PlayerPrefs.GetInt("highScore",0);
+         bestScore.text = "" + highScore;
+         
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0,targets.Count);
+            Instantiate(targets[index]);
+        }
+        
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {   score += scoreToAdd;
+        scoreText.text =  "Score: " +score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        RestartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void StartGame(int difficulty)
+    {   
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnEnemy());
+        UpdateScore(0);
+
+        TitleScreen.gameObject.SetActive(false);
+    }
+
+    public void BestScore()
+    {
+        if(score > highScore)
+        {
+            PlayerPrefs.SetInt("highScore",score);
+            bestScore.text = "" +score;
+            Debug.Log("Score:" + score);
+            PlayerPrefs.Save();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BestScore();
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRigid;
+    private GameManager gameManager;
+    private float minSpeed = 11f;
+    private float maxSpeed = 15f;
+    private float maxTorque = 10f;
+    private float xRange = 4f;
+    private float ySpawnPos = -2f;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRigid = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+       
+        targetRigid.AddForce(RandomForce(),ForceMode.Impulse);
+        targetRigid.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(),ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+     private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            Destroy(this.gameObject);
+            Instantiate(explosionParticle , transform.position,explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        Destroy(this.gameObject);
+        if(this.pointValue > 0)
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    private Vector3 RandomForce()
+    {
+       return Vector3.up * Random.Range(minSpeed,maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+       return Random.Range(-maxTorque,maxTorque);
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange,xRange), ySpawnPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
